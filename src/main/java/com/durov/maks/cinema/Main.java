921b984a26c46baf9d2162cd7c4c6419bd1b95ac
maks45 <maksim.durov45@gmail.com>
2020-06-14 11:58:18 +0300

    added dependencies

--- src/main/java/com/durov/maks/cinema/Main.java
+++ src/main/java/com/durov/maks/cinema/Main.java
@@ -17,6 +17,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Main {
     public static void main(String[] args) {
+
         AnnotationConfigApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         Movie movie = new Movie();
