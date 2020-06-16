39c08488c5a34d982e4cdf00d1e396498b50c1b6
maks45 <maksim.durov45@gmail.com>
2020-06-16 13:43:28 +0300

    added injectDataController

--- src/main/java/com/durov/maks/cinema/controller/InjectDataController.java
+++ src/main/java/com/durov/maks/cinema/controller/InjectDataController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-public class HelloController {
+@RequestMapping("/inject-data")
+public class InjectDataController {
 
     @GetMapping
-    public String sayHello() {
-        return "hello";
+    public String injectData() {
+        return "data injected";
     }
 }
